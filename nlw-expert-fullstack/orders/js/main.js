bc4367e98e5206173fa01ee449800224b531bcda
Raphael Gregg <raphaelgregg@gmail.com>
2024-02-15 18:03:12 -0300

    refactor: create webcomponent header

--- nlw-expert-fullstack/orders/js/main.js
+++ nlw-expert-fullstack/orders/js/main.js
@@ -1,40 +1,26 @@
-import { CATEGORIES, MENU } from '../utils/data/products.js';
 
-const categories = document.querySelector('#categories');
-const templateCategories = document.querySelector('#template-categories');
-
-const products = document.querySelector('#products');
-const templateProducts = document.querySelector('#template-products');
-
-// Create component list category
-const categoryList = templateCategories.content.cloneNode(true);
-
-for(let category of CATEGORIES) {  
-  const li = categoryList.querySelector('ul li').cloneNode(true);
-  li.querySelector('a').textContent = category
-  li.querySelector('a').href = `#${category}`
-  
-  categoryList.querySelector('ul').appendChild(li);
-}
-
-categoryList.querySelector('ul li').remove();
-categories.appendChild(categoryList);
-
-// Create component card list
-for(const item of MENU) {
-  const productItem = templateProducts.content.cloneNode(true);
-  productItem.querySelector('h4').textContent = item.title;
-  productItem.querySelector('h4').setAttribute('id', item.title)
-
-  for(let product of item.data) {
-    const dt = productItem.querySelector('dl dt').cloneNode(true);
-    dt.querySelector('img').src = product.thumbnail;
-    dt.querySelector('p').textContent = product.description;
-    dt.querySelector('.title').textContent = product.title;
-    productItem.querySelector('dl').appendChild(dt);
+document.addEventListener("DOMContentLoaded", function(){
+  let includes = document.getElementsByTagName('include');
+  for(var i=0; i<includes.length; i++){
+    let include = includes[i];
+    load_file(includes[i].attributes.src.value, function(text){
+      include.insertAdjacentHTML('afterend', text);
+      include.remove();
+    });
   }
+  function load_file(filename, callback) {
+    fetch(filename).then(response => response.text()).then(text => callback(text));
+  }
+});
 
-  productItem.querySelector('dl dt').remove();
-  
-  products.appendChild(productItem);
-}
+
+const header = document.querySelector('#header');
+const templateHeader = document.querySelector('#template-header');
+const headerClone = templateHeader.content;
+header.appendChild(headerClone);
+
+console.log(templateHeader)
+
+// METODO para INCLUDE HTML
+
+
